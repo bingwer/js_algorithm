42bbbed41a108a0716d7dbf8e6549441155bf171
Seunghyo Lee <bingwer@naver.com>
2023-04-06 23:12:48 +0900

    프로그래머스 문제풀이 : 부족한 금액 계산하기

---
+++ programmers/lv1/부족한 금액 계산하기.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+  let totalPrice = 0;
+
+  for (let i = 1; i <= count; i++) {
+    totalPrice += price * i;
+  }
+
+  return totalPrice <= money ? 0 : totalPrice - money;
+}
+
+console.log(solution(3, 20, 4)); // 10
